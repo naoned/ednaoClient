ccec3076f1e9556e6c7dd0746899a95628b49f62
vbeauvivre <vbeauvivre@naoned.fr>
2015-11-20 10:05:46 +0100

    First commit : add simple iframe file

---
+++ iframe.php
@@ -0,0 +1,5 @@
+
+<iframe src="http://vbeauvivre.insitu.help.mnesys.fr" width="300" height="300" id="ednao">
+  <p>Votre navigateur ne supporte pas l'élément iframe, l’aide ne peut donc pas être affichée</p>
+</iframe>
+
