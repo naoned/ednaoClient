e11e920ac3ce159c6506c4a675cd03008e6be3dd
vbeauvivre <vbeauvivre@naoned.fr>
2015-11-20 15:11:19 +0100

    Add url of ednao help site as an argument

--- iframe.php
+++ iframe.php
@@ -7,19 +7,20 @@ namespace Naoned\ednaoClient;
 */
 class Iframe
 {
-    public function render($socle, $version, $product, $group, $accessLevel, $page)
+    public function render($url, $socle, $version, $product, $group, $accessLevel, $page)
     {
         $token = 'test';
         return sprintf(
             '
             <iframe
-                src="http://vbeauvivre.insitu.help.mnesys.fr/%s/%s/%s"
+                src="%s"
                 frameborder="1" marginheight="0" marginwidth="0"
                 width="400" height="600"
                 id="ednao"
             >
               <p>Votre navigateur ne supporte pas l’élément iframe, l’aide ne peut donc pas être affichée</p>
             </iframe>',
+            $url,
             $token,
             $version,
             $page
